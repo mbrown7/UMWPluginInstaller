9ab8e0b2a4758796d65e49967636e54f6ade9926
Frankie DePaola <depaolafv@gmail.com>
2014-03-12 14:35:40 -0400

    Added License info
    As recommended by Wordpress' plugin tutorial

--- pluginstaller.php
+++ pluginstaller.php
@@ -4,4 +4,20 @@
  * Description: Allows fast and easy installation of predetermined packages of plugins/themes/etc. for Wordpress
  * Version: 0.1
  * Author: Morgan Brown, Frankie DePaola, Logan Wholey
+ * License: GPL2
  */
+ /*  Copyright 2014  Morgan Brown, Frank DePaola, Logan Wholey  (email : mbrown7@mail.umw.edu, depaolafv@gmail.com, lwholeyumw@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
