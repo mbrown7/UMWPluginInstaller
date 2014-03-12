4b78416b1cdf5798037c0b1ad3edd41c77462dcc
Frankie DePaola <depaolafv@gmail.com>
2014-03-12 14:29:39 -0400

    Here's a php file
    I'm committing something! Required header for Wordpress plugins.
    Assuming our plugin will consist entirely of one file, let this be it.
    Name subject to change, of course.

---
+++ pluginstaller.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Plugin Name: UMW PlugInstaller
+ * Description: Allows fast and easy installation of predetermined packages of plugins/themes/etc. for Wordpress
+ * Version: 0.1
+ * Author: Morgan Brown, Frankie DePaola, Logan Wholey
+ */
