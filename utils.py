f1a3b5bc5cfe9e162dbe985dedf357cb39f78215
Logan Wholey <lwholey@mail.umw.edu>
2014-03-30 14:53:46 +0000

    forms

---
+++ utils.py
@@ -0,0 +1,10 @@
+# utils.py
+import MySQLdb
+
+DATABASE='session'
+DB_USER = 'assist'
+DB_PASSWORD = 'assist'
+HOST = 'localhost'
+
+def db_connect():
+	return MySQLdb.connect(HOST, DB_USER, DB_PASSWORD, DATABASE)
